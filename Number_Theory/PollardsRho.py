f5be1ba27007dbc2aa2319cba15b04f8acacfcfd
matteosegonnes <segonnes.64matteo@gmail.com>
2021-11-01 00:55:56 +0100

    Simple curiosity, seems to work

---
+++ Number_Theory/PollardsRho.py
@@ -0,0 +1,30 @@
+import math
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def pollards_rho(n):
+    if n % 2 == 0:
+        return 2
+    x = 2
+    y = 2
+    d = 1
+    f = lambda x: (x*x + 1) % n
+    while d == 1:
+        x = f(x)
+        y = f(f(y))
+        d = gcd(abs(x - y), n)
+    if d == n:
+        return None
+    return d
+
+#try
+n = 8051
+factor = pollards_rho(n)
+print("A non-trivial factor of", n, "is", factor)
+
+
+
+

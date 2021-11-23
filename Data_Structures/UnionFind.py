ef06a980025d898e618bcd6639b1fbb60df60846
matteosegonnes <segonnes.64matteo@gmail.com>
2021-11-23 23:06:08 +0100

    UnionFind ENSIMAG for Algo project

---
+++ Data_Structures/UnionFind.py
@@ -0,0 +1,23 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        self.rank = [0] * size
+
+    def find(self, item):
+        if self.parent[item] != item:
+            self.parent[item] = self.find(self.parent[item])  # Path compression
+        return self.parent[item]
+
+    def union(self, set1, set2):
+        root1 = self.find(set1)
+        root2 = self.find(set2)
+        if root1 != root2:
+            # Union by rank
+            if self.rank[root1] > self.rank[root2]:
+                self.parent[root2] = root1
+            elif self.rank[root1] < self.rank[root2]:
+                self.parent[root1] = root2
+            else:
+                self.parent[root2] = root1
+                self.rank[root1] += 1
+

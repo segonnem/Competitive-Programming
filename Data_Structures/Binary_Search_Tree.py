31dbbd2d49fe18646333f182e5e98beba4b02bf6
matteosegonnes <segonnes.64matteo@gmail.com>
2022-01-04 19:23:00 +0100

    Binary Search Tree add methods + test

--- Data_Structures/Binary_Search_Tree.py
+++ Data_Structures/Binary_Search_Tree.py
@@ -27,7 +27,7 @@ class BinarySearchTree:
             else:
                 self._insert(node.right, key, value)
         else:
-            # If the key already exists, update the value
+            
             node.value = value
 
     def search(self, key):
@@ -53,6 +53,46 @@ class BinarySearchTree:
             result.append((node.key, node.value))
             result += self._inorder_traversal(node.right)
         return result
+    
+    def height(self):
+        return self._height(self.root)
+
+    def _height(self, node):
+        if node is None:
+            return 0
+        else:
+            return 1 + max(self._height(node.left), self._height(node.right))
+    
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return None
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            # Node with only one child or no child
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+
+            # Node with two children, get the inorder successor
+            min_larger_node = self._min_value_node(node.right)
+            node.key = min_larger_node.key
+            node.value = min_larger_node.value
+            node.right = self._delete(node.right, min_larger_node.key)
+        return node
+
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
 
 
 bst = BinarySearchTree()
@@ -67,3 +107,9 @@ bst.insert(80, 'Value at 80')
 print("In-order Traversal:", bst.inorder_traversal())
 print("Search for key 30:", bst.search(30))
 print("Search for key 90:", bst.search(90))  
+print("Height of the tree:", bst.height())
+
+bst.delete(30) 
+bst.delete(70)
+bst.delete(50)  
+print("In-order Traversal after deleting the root (50):", bst.inorder_traversal())

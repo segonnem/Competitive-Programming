48d90fbe0050f9c6bf1f3afd59d9e0efeb5161ae
matteosegonnes <segonnes.64matteo@gmail.com>
2022-06-08 15:57:10 +0200

    Convex Hull RO ENSIMAG

---
+++ Geometry/Convex_Hull.py
@@ -0,0 +1,30 @@
+def convex_hull(points):
+    points = sorted(points)
+
+    if len(points) <= 1:
+        return points
+
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    # Build lower hull 
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    
+    return lower[:-1] + upper[:-1]
+
+
+points = [(0, 3), (1, 1), (2, 2), (4, 4), (0, 0), (1, 2), (3, 1), (3, 3)]
+print("Convex hull of points is:", convex_hull(points))
+# Go check si ca marche
